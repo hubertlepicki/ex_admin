ad1e03c17f8d7d1f5a3e96736fd3f112bb5b4b36
Hubert Łępicki <hubert.lepicki@amberbit.com>
2018-10-16 09:46:30 +0200

    Updates for Ecto 3.0

--- lib/ex_admin/table.ex
+++ lib/ex_admin/table.ex
@@ -194,21 +194,6 @@ defmodule ExAdmin.Table do
         Map.get(table_opts, :filter, ""))
     end
   end
-  def handle_contents(%Ecto.DateTime{} = dt, field_name) do
-    td class: to_class("td-", field_name) do
-      text to_string(dt)
-    end
-  end
-  def handle_contents(%Ecto.Time{} = dt, field_name) do
-    td class: to_class("td-", field_name) do
-      text to_string(dt)
-    end
-  end
-  def handle_contents(%Ecto.Date{} = dt, field_name) do
-    td class: to_class("td-", field_name) do
-      text to_string(dt)
-    end
-  end
   def handle_contents(%{}, _field_name), do: []
   def handle_contents(contents, field_name) when is_binary(contents) do
     td(to_class(".td-", field_name)) do
